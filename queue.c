787a654b012dbf7ab7a996da26fde1bbaeb9334c
leo-hsieh <leo857366@gmail.com>
2025-03-28 22:34:51 +0800

    Implement q_remove_head and q_remove_tail
    Check the queue is empty or not before remove the element.
    Copy the value in the element and delete the node.
    
    Change-Id: I25043d8e9f50bc9ee717a8e78e91fa70c4c2e111

--- queue.c
+++ queue.c
@@ -56,13 +56,27 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (list_empty(head)) {
+        return NULL;
+    }
+    element_t *entry;
+    entry = container_of(head->next, element_t, list);
+    strlcpy(sp, entry->value, sizeof(entry->value));
+    list_del(head->next);
+    return entry;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (list_empty(head)) {
+        return NULL;
+    }
+    element_t *entry;
+    entry = container_of(head->prev, element_t, list);
+    strlcpy(sp, entry->value, sizeof(entry->value));
+    list_del(head->prev);
+    return entry;
 }
 
 /* Return number of elements in queue */
